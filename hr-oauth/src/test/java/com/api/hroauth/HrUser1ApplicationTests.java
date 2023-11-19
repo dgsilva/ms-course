d5f8e4f472581818d85ec6bb054d6a25e0e2ecd8
dgsilva <diegorj93@gmail.com>
2023-11-19 18:18:55 -0300

    project hr-oauth created.

---
+++ hr-oauth/src/test/java/com/api/hroauth/HrUser1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUser1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
